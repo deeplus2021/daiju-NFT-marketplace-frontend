37608738174a4f51717d83b619bda176f878e492
biginfo2012 <biginfo2012@gmail.com>
2024-06-13 03:34:03 +0200

    create nft: upload ipfs img using pinata

--- context/constants.js
+++ context/constants.js
@@ -1,4 +1,4 @@
 import marketAbi from './NFTMarketplace.json';
 
-export const MarketAddress = '0xF5f6B924332C350E3Fcd3A50Fc94db822f0B760f';
+export const MarketAddress = '0x1e947ee2d808d5aE673b7AD8ED2BDf42Bd16175a';
 export const MarketAddressABI = marketAbi.abi;
